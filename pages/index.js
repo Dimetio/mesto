7afd9911b91d3599851cc9f884f22196bf330075
ddubinin <dubinin.dmitry@mail.ru>
2022-01-14 13:28:27 +0300

    update project

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,54 @@
 (function () {
+  const page = document.querySelector('.page'); // body
   const popup = document.querySelector('.popup'); // поп-ап
   const form = popup.querySelector('.popup__container'); // форма
   const inputName = popup.querySelector('.popup__input-name'); // инпут с именем
   const inputJob = popup.querySelector('.popup__input-job'); // инпус с деятельностью
-  const infoName = document.querySelector('.profile-info__name');
-  const infoJob = document.querySelector('.profile-info__job');
+  const infoName = document.querySelector('.profile-info__name'); // поле с именем
+  const infoJob = document.querySelector('.profile-info__job'); // поле с деятельностью
 
-  const btnClose = popup.querySelector('.popup__btn-close');
-  const btnEdit = document.querySelector('.profile-info__edit'); // кнопка редактирования профиля
+  const btnClose = popup.querySelector('.popup__btn-close'); // крестик
+  const btnEdit = document.querySelector('.profile-info__edit'); // кнопка редактирования
 
-  let opened = false; // флаг переключения
+  let isOpened = false; // флаг переключения
 
-  document.addEventListener('keydown', (event) => {
-    if (opened && event.keyCode === 27) {
+  // закрыть поп-ап по нажатию на Esc
+  page.addEventListener('keydown', (event) => {
+    if (isOpened && event.keyCode === 27) {
       toggleClass(popup, 'popup_opened');
+      toggleClass(page, 'popup-visible');
     }
   });
 
+  // функция тоглит класс
   const toggleClass = (elem, className) => {
     elem.classList.toggle(className);
-
-    opened ? opened = !opened : opened = !opened; // переключаение флага
-
-    // if (opened) {
-    //   opened = !opened;
-    // } else {
-    //   opened = !opened;
-    // }
+    isOpened = false;
   }
 
   btnClose.addEventListener('click', () => {
     toggleClass(popup, 'popup_opened');
+    toggleClass(page, 'popup-visible');
   })
 
   btnEdit.addEventListener('click', () => {
     //popup.classList.add('popup_opened');
     toggleClass(popup, 'popup_opened');
+    toggleClass(page, 'popup-visible');
 
     inputName.value = infoName.textContent;
     inputJob.value = infoJob.textContent;
+
+    isOpened = true;
   });
 
+  // закрываю поп-ап по клику вне поп-апа
   popup.addEventListener('click', (e) => {
     let target = e.target;
 
     if (target === popup) {
       toggleClass(popup, 'popup_opened');
+      toggleClass(page, 'popup-visible');
     }
   })
 
@@ -57,6 +60,7 @@
 
     //popup.classList.remove('popup_opened');
     toggleClass(popup, 'popup_opened');
+    toggleClass(page, 'popup-visible');
   }
 
   form.addEventListener('submit', formSubmitHandler)
