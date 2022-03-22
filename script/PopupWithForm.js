27b0e1bf39e502480bc1b2d0c3ffcab1958b3a64
ddubinin <dubinin.dmitry@mail.ru>
2022-03-22 18:03:42 +0300

    feat: create class PopupWithForm

---
+++ script/PopupWithForm.js
@@ -0,0 +1,40 @@
+import Popup from "./Popup.js";
+
+export default class PopupWithForm extends Popup {
+  constructor(popupSelector, submitForm) {
+    super(popupSelector);
+    this._from = this._popup.querySelector('.popup__form');
+    this._submitForm = submitForm;
+    this._boundHandleSubmit = this._handleSubmit.bind(this);
+  }
+
+  _handleSubmit(e) {
+    this._submitForm(e, this._getInputValues());
+  }
+
+  _getInputValues() {
+    this._fromValues = {};
+
+    this._from.querySelectorAll('.popup__input').forEach(input => {
+      this._fromValues[input.name] = input.value;
+    });
+
+    return this._fromValues;
+  }
+
+  close() {
+    super.close();
+    this._from.reset();
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._from.addEventListener('submit', this._boundHandleSubmit)
+  }
+
+  removeListeners() {
+    super.removeListeners();
+
+    this._from.removeEventListener('submit', this._boundHandleSubmit)
+  }
+}
