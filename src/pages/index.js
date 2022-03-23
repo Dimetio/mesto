001a108e025d6228ba947c6d73f92b29be24e75d
ddubinin <dubinin.dmitry@mail.ru>
2022-03-23 15:16:02 +0300

    fix: init updates in classes

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,12 +25,11 @@ const validatorEdit = new FormValidator(formParams, formEdit);
 validatorEdit.enableValidation();
 
 const cardList = new Section({
-  data: initialCards,
   renderer: (item) => {
     cardList.addItem(createCard(item));
   }
 }, '.cards');
-cardList.renderItems();
+cardList.renderItems(initialCards);
 
 const popupFullscreen = new PopupWithImage('.popup-fullscreen');
 const popupAdd = new PopupWhithFrom('.popup-add', handleAddFormSubmit);
@@ -40,13 +39,12 @@ const userElement = new UserInfo('.profile-info__name', '.profile-info__job');
 /* functions */
 
 function createCard(data) {
-  const card = new Card(data, '.card-template', openFullscreenPopup);
+  const card = new Card(data, '.card-template', (title, link) => popupFullscreen.open(title, link));
   return card.generateCard();
 }
 
 /* обработчик для добавления */
 function handleAddFormSubmit(e, data) {
-  e.preventDefault();
   cardList.addItem(createCard(data))
   popupAdd.close();
 }
@@ -58,21 +56,12 @@ function handleEditFormSubmit(e, data) {
   popupEdit.close();
 }
 
-function openFullscreenPopup(e) {
-  const card = e.target.closest('.card');
-  const title = card.querySelector('.card__title').textContent;
-  const link = card.querySelector('.card__img').src;
-
-  popupFullscreen.open(title, link);
-}
-
 function openEditPopup() {
   const userInfo = userElement.getUserInfo();
   inputName.value = userInfo.name;
   inputJob.value = userInfo.job;
 
   validatorEdit.updateValidation();
-
   popupEdit.open();
 }
 
