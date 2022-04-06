c707e3d0701962987f2b221a46da447f1ccf21f6
ddubinin <dubinin.dmitry@mail.ru>
2022-04-06 13:40:24 +0300

    feat: creat class PopupWithSubmit

---
+++ src/components/PopupWithSubmit.js
@@ -0,0 +1,21 @@
+import Popup from './Popup.js';
+
+export default class PopupWithSubmit extends Popup {
+  constructor(popupSelector) {
+    super(popupSelector);
+    this._form = this._popup.querySelector('.popup__form');
+  }
+
+  setFormSubmitHandler(handler) {
+    this.setFormSubmitHandler = handler;
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._form.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      this.setFormSubmitHandler();
+    });
+  }
+}
