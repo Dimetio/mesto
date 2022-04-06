8379760a9a9f92bacf8f08e2eace1c01227cf043
ddubinin <dubinin.dmitry@mail.ru>
2022-04-06 13:45:09 +0300

    fix: refactor declaration variable

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,40 +1,20 @@
-export const initialCards = [{
-  title: 'Архыз',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-},
-{
-  title: 'Челябинская область',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-},
-{
-  title: 'Иваново',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-},
-{
-  title: 'Камчатка',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-},
-{
-  title: 'Холмогорский район',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-},
-{
-  title: 'Байкал',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-}
-];
-  
 /* popup-edit */
-const popupEdit = document.querySelector('.popup-edit');
-export const formEdit = popupEdit.querySelector('.popup__form');
-export const inputName = popupEdit.querySelector('.popup__input_name');
-export const inputJob = popupEdit.querySelector('.popup__input_job');
-/* popup-add */
-const popupAdd = document.querySelector('.popup-add');
-export const formAdd = popupAdd.querySelector('.popup__form');
+export const inputName = document.querySelector('.popup__input_name');
+export const inputAbout = document.querySelector('.popup__input_about');
+
+/* forms */
+export const formAdd = document.forms['popup-add'];
+export const formEdit = document.forms['popup-edit'];
+export const formAvatar = document.forms['popup-avatar'];
+
 /* profile */
+export const avatarImg = document.querySelector('.profile-info__avatar');
 export const btnEdit = document.querySelector('.profile-info__edit');
 export const btnAdd = document.querySelector('.profile__add');
+
+/* api data */
+export const url = 'https://mesto.nomoreparties.co/v1/cohort-39';
+export const token = '006bb176-a176-4c97-bbc5-cf71d72d94bc';
 
 export const formParams = {
   inputSelector: '.popup__input',
@@ -43,4 +23,3 @@ export const formParams = {
   inputErrorClass: 'popup__input_error',
   errorClass: 'popup__input-error_active'
 };
-
