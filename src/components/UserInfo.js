82966d98edf98ae1dd7a11a37d46355baa2890a1
ddubinin <dubinin.dmitry@mail.ru>
2022-04-06 18:47:24 +0300

    feat: add new method in class UserInfo

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,17 +1,22 @@
 export default class UserInfo {
-  constructor(nameSelector, aboutSelector) {
+  constructor(nameSelector, aboutSelector, avatarSelector) {
     this._userName = document.querySelector(nameSelector);
     this._userAbout = document.querySelector(aboutSelector);
+    this._avatar = document.querySelector(avatarSelector);
+  }
+
+  updateAvatar(data) {
+    this._avatar.style.backgroundImage = `url(${data.avatar})`;
   }
 
   getUserInfo() {
     return {
       name: this._userName.textContent,
-      about: this._userAbout.textContent
+      about: this._userAbout.textContent,
     }
   }
 
-  setUserInfo({ name, about }) {
+  setUserInfo({ name, about, avatar }) {
     this._userName.textContent = name;
     this._userAbout.textContent = about;
   }
